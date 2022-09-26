a58093b8028c8a6af3bb860c901f552a0271b65f
jdanderson2 <jeo.anderson2@gmail.com>
2022-09-26 14:52:16 -0500

    initial commit

---
+++ lib/ssl_certificate_checker.ex
@@ -0,0 +1,45 @@
+defmodule SslCertificateChecker do
+  def check(host, port \\ 443) do
+    command = command(host, port)
+    case System.cmd("/bin/sh", ["-c", command]) do
+      {output, 0} ->
+              output
+              |> format_output
+              |> output_to_map(%{})
+        _-> "unable to load certificate"
+
+    end
+  end
+
+defp format_output(output) do
+  output
+  |> String.split("\n", trim: true)
+end
+
+defp output_to_map([], map), do: map
+defp output_to_map([out | output], map) do
+      arr = out
+              |>String.replace("=", "->", global: false)
+              |>String.split("->", trim: true)
+
+      output_to_map(output,
+              map
+              |>Map.merge(get_key_val(arr))
+            )
+end
+
+defp  get_key_val([key, val]) when key == "subject" or key == "issuer" do
+  case Regex.run(~r/O=(.*?)\//, val) do
+          nil -> %{ key => "" }
+          arr -> %{ key => Enum.at(arr, 1) }
+  end
+
+
+end
+
+defp get_key_val([key, val]), do: %{ key => val }
+
+defp command(host, port) do
+      "echo | openssl s_client -shocerts -connect #{host}:#{port} 2>/dev/null | openssl x509 -inform pem -noout -issuer -subject -dates"
+end
+end
---
+++ test/ssl_certificate_checker_test.exs
@@ -0,0 +1,8 @@
+defmodule SslCertificateCheckerTest do
+  use ExUnit.Case
+  doctest SslCertificateChecker
+
+  test "greets the world" do
+    assert SslCertificateChecker.hello() == :world
+  end
+end
